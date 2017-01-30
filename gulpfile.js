f4d88fccd1d18e9c82ebca49a2afda6ccfd6c97b
Josivan de Souza [dell] <josivan@josivan.com.br>
2017-01-30 01:20:31 -0200

    Trying to avoid webpack.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const
+  gulp  = require('gulp'),
+  browserify  = require('browserify'),
+  tsify       = require('tsify'),
+  source      = require('vinyl-source-stream'),
+  ts    = require('gulp-typescript'),
+  babel = require('gulp-babel');
+
+gulp.task('ts-babel', () => {
+  var tsProject = ts.createProject(__dirname + "/tsconfig.json");
+  var tsResult = gulp.src("src/**/*.ts").pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest("release"));
+});
+
+gulp.task('default', () => {
+  return browserify({
+      basedir: '.',
+      debug: true,
+      entries: ['src/app/app.ts'],
+      cache: {},
+      packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
+});
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: {
-    app: 'src/app/app.ts'
-  },
-  output: {
-    filename: 'app.js'
-  }
-}
